20c70ee523e15fd66a09bebe268b6f113fddea60
Vit Hanousek <vithanousek@seznam.cz>
2018-10-12 10:56:22 +0200

    Add reduced configuration for running on linux without simulation.

---
+++ cmake/configs/posix_linux_native.cmake
@@ -0,0 +1,49 @@
+
+set(CMAKE_TOOLCHAIN_FILE ${PX4_SOURCE_DIR}/cmake/toolchains/Toolchain-native.cmake)
+
+set(config_module_list
+
+
+	drivers/batt_smbus
+	drivers/vmount
+
+	#
+	# System commands
+	#
+	#systemcmds/bl_update
+	#systemcmds/config
+	#systemcmds/dumpfile
+	#systemcmds/esc_calib
+	#systemcmds/led_control
+	#systemcmds/mixer
+	#systemcmds/motor_ramp
+	#systemcmds/mtd
+	#systemcmds/nshterm
+	systemcmds/param
+	systemcmds/perf
+	#systemcmds/pwm
+	systemcmds/reboot
+	systemcmds/shutdown
+	#systemcmds/sd_bench
+	systemcmds/top
+	systemcmds/topic_listener
+	systemcmds/tune_control
+	systemcmds/ver
+
+
+	#
+	# General system control
+	#
+	modules/mavlink
+
+	#
+	# Logging
+	#
+	modules/logger
+
+	#
+	# Library modules
+	#
+	modules/dataman
+
+)
