1237bd904884ef2a119bf3a4f70dec7eb4ff72f1
Roman Dvořák <romandvorak@mlab.cz>
2020-12-18 21:38:10 +0100

    minimalizace datoveho toku

--- src/modules/mavlink/mavlink_main.cpp
+++ src/modules/mavlink/mavlink_main.cpp
@@ -1739,12 +1739,12 @@ Mavlink::configure_streams_to_default(const char *configure_single_stream)
 	case MAVLINK_MODE_CUSTOM:
 		/* HEARTBEAT is constant rate stream, rate never adjusted */
 		configure_stream_local("HEARTBEAT", 1.0f);
-		configure_stream_local("GPS_RAW_INT", 0.5f);
-		configure_stream_local("ALTITUDE", 0.5f);
-		configure_stream_local("ATTITUDE", 0.5f);
-		configure_stream_local("GLOBAL_POSITION_INT", 0.5f);
-		configure_stream_local("SYS_STATUS", 0.1f);
-		configure_stream_local("VFR_HUD", 0.5f);
+		configure_stream_local("GPS_RAW_INT", 1.0f);
+		// configure_stream_local("ALTITUDE", 0.5f);
+		// configure_stream_local("ATTITUDE", 0.5f);
+		// configure_stream_local("GLOBAL_POSITION_INT", 0.5f);
+		// configure_stream_local("SYS_STATUS", 0.1f);
+		// configure_stream_local("VFR_HUD", 0.5f);
 		break;
 
 	case MAVLINK_MODE_CONFIG: // USB
