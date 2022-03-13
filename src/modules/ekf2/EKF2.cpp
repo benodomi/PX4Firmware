2bbad956e1886964920ba0e4e317bdc82c9186fc
Dominik Beno <dominik.beno7@gmail.com>
2022-03-13 23:28:09 +0100

    Added debug variables into vehicle_local_position. Added new variable dist_bottom_row, which is directly compensated sensor measurment by transormation matrix body/Earth.

--- src/modules/ekf2/EKF2.cpp
+++ src/modules/ekf2/EKF2.cpp
@@ -930,10 +930,13 @@ void EKF2::PublishLocalPosition(const hrt_abstime &timestamp)
 	// Distance to bottom surface (ground) in meters
 	// constrain the distance to ground to _rng_gnd_clearance
 	lpos.dist_bottom = math::max(_ekf.getTerrainVertPos() - lpos.z, _param_ekf2_min_rng.get());
+	lpos.dist_bottom_raw = _ekf.GetCompensatedDistance();
 	lpos.dist_bottom_valid = _ekf.isTerrainEstimateValid();
 	lpos.dist_bottom_sensor_bitfield = _ekf.getTerrainEstimateSensorBitfield();
 	lpos.aid_suitable = _ekf.getRangeAidSuitable();
 	lpos.is_data_healthy = _ekf.DataHealthy();
+	lpos.is_ready = _ekf.Ready();
+	lpos.is_healthy = _ekf.Healthy();
 
 	if (!_had_valid_terrain) {
 		_had_valid_terrain = lpos.dist_bottom_valid;
